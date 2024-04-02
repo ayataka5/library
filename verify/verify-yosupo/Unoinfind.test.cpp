fce285b2a4f982cf6abed013b80e598d94c57a65
ayataka5 <ayataka.cp@gmail.com>
2024-04-03 02:15:09 +0900

    Update

---
+++ verify/verify-yosupo/Unoinfind.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../../data-structure/unionfind.hpp"
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+    UnionFind uf(N);
+    while(Q--) {
+        int t, u, v;
+        cin >> t >> u >> v;
+        if(t == 0) uf.unite(u, v);
+        if(t == 1) cout << (uf.same(u, v)) << endl;
+    }
+    return 0;
+}
